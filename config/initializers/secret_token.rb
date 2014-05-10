3ece1423b1acbe3230ec6456ebd2b2bf94125eeb
Edi Costa <edi.costa.dev@gmail.com>
2014-05-10 17:50:27 +0100

    Pre Alpha Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Euphexya::Application.config.secret_key_base = '115a03e7096a720df1978a0790282c6d3a0764dd1f69bbfc35635e04a6bc37548e83f36928dc3c641644583b0f3ed5a18cbc4a1ff84c70e0048f61e98d8ad428'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Euphexya::Application.config.session_store :cookie_store, key: '_euphexya_session'
