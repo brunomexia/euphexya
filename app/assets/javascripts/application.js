bee38039eacbd23e0911fcd2ec6d92aa8aa11697
Edi Costa <edi.costa.dev@gmail.com>
2014-05-11 17:39:17 +0100

    Slider and Loader progress

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,59 @@
 
 $(document).ready(function(){
 	// Code
+	
+    $('.loader div').css('margin-top', $(window).height()/2 - $('.loader div').outerHeight()/2);
 });
 
 $(window).load(function() {
-	
+
+	// Body Positioning
+	var mhHeight = $('.mainhead').outerHeight();
+	var windowHeight = $(window).height();
+	var homedesHeight = $('.home-destaque').outerHeight();
+
+	$('.body').css('margin-top', mhHeight);
+	$('.cycle-slide').height(windowHeight - mhHeight - homedesHeight);
 	// Load animation
 	setTimeout(function(){
 		$('.loader div').css({'margin-top': 0, 'opacity': 0});
 		$('.loader').delay(500).fadeOut(150);
-	}, 185);
+	}, 1585);
+
+
+
+
+
+
+	var progress = $('#progress'),
+        slideshow = $( '.home_banner_cycle .cycle-slideshow' );
+
+    slideshow.on( 'cycle-initialized cycle-before', function( e, opts ) {
+        progress.stop(true).css( 'width', 0 );
+    });
+
+    slideshow.on( 'cycle-initialized cycle-after', function( e, opts ) {
+        if ( ! slideshow.is('.cycle-paused') )
+            progress.animate({ 'width' : slideshow.width() }, opts.timeout, 'linear' );
+    });
+
+    slideshow.on( 'cycle-paused', function( e, opts ) {
+       progress.stop(); 
+    });
+
+    slideshow.on( 'cycle-resumed', function( e, opts, timeoutRemaining ) {
+        progress.animate({ 'width' : slideshow.width() }, timeoutRemaining, 'linear' );
+    });  
+    
+    
+    $(".cycle-togglePause").toggle(function(){
+        console.log('paused');
+        slideshow.cycle("pause");
+        $(".cycle-togglePause").text('► ');
+    },function(){
+        console.log('resumed');
+        slideshow.cycle("resume");
+        $(".cycle-togglePause").text('||');
+    });
 
 });
