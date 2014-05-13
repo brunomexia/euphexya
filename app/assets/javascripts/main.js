7969fa1ef0cd400d6a0591583634aa40590d0c44
Edi Costa <edi.costa.dev@gmail.com>
2014-05-13 14:47:22 +0100

    Turbolinks disabled in the homepage link to prevent slider fuck up (Finally). Css Altered.

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,53 @@
+$(function() {    
+
+    //HOMEPAGE JS
+    if ($("#welcome.index").length) {
+
+        var progress = $('#progress'),
+            slideshow = $( '.cycle-slideshow' );
+
+        slideshow.on( 'cycle-initialized cycle-before', function( e, opts ) {
+            progress.stop(true).css( 'width', 0 );
+        });
+
+        slideshow.on( 'cycle-initialized cycle-after', function( e, opts ) {
+            if ( ! slideshow.is('.cycle-paused') )
+                progress.animate({ width: '100%' }, opts.timeout, 'linear' );
+        });
+
+        slideshow.on( 'cycle-paused', function( e, opts ) {
+           progress.stop(); 
+        });
+
+        slideshow.on( 'cycle-resumed', function( e, opts, timeoutRemaining ) {
+            progress.animate({ width: '100%' }, timeoutRemaining, 'linear' );
+        });
+    	
+    	$(document).ready(function(){
+
+    		// Center Loader 
+    		$('.loader div').css('margin-top', $(window).height()/2 - $('.loader div').outerHeight()/2);
+
+    		// Body Positioning
+    		var windowWidth = $(window).width(),
+                windowHeight = $(window).height(),
+                mhHeight = $('.mainhead').outerHeight(),                
+                homedesHeight = $('.home-destaque').outerHeight();
+
+            // Proper Height Space
+            $('.body').css('margin-top', mhHeight);
+            // Proper Slider Height
+            $('.cycle-slide').css('height', windowHeight - mhHeight - homedesHeight);
+            // Assign respective DIV Backgrounds
+            $('.cycle-slide').each(function() {
+                var imgBg = $(this).attr('data-image-insert');
+                $(this).css('background', 'url("' + imgBg + '")');
+            });
+            
+        });
+
+        
+    
+    }
+
+});
--- app/assets/javascripts/welcome.js.coffee
+++ app/assets/javascripts/welcome.js.coffee
@@ -1,3 +1,11 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$(window).on "load", ->
+  run = () ->
+    $('.loader div').css({'margin-top': 0, 'opacity': 0})
+    $('.loader').delay(500).fadeOut(250)
+
+  setTimeout(run, 150)
